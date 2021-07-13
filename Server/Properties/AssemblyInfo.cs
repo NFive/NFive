e0e63e05111619d07d38f1910ef11737398f01f0
Joe Biellik <joebiellik@gmail.com>
2021-07-14 00:46:43 +0100

    Cleanup

--- Server/Properties/AssemblyInfo.cs
+++ Server/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NFive")]
 [assembly: AssemblyProduct("NFive Server")]
-[assembly: AssemblyCopyright("Copyright © NFive 2018-2019")]
+[assembly: AssemblyCopyright("Copyright © NFive 2018-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
